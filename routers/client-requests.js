3f60f77e7c29a808a66ea1f8392ef3d3895ec0bf
TaoufiqBenallah <taoufiq.benallah@gmail.com>
2022-03-30 16:28:35 +0000

    kookjhh

--- routers/client-requests.js
+++ routers/client-requests.js
@@ -47,7 +47,7 @@ router.post('/campaign-offer-data', async (req, res) => {
 
         const dataD = await sfmcAPI.getCampaignOfferTypes(req.body.access_token)
 
-        const campaignsOffersTypes = dataD.data;
+        const campaignsOffersTypes = dataD.data.items.values;
 
         res.status(200).json({ data: campaignsOffersTypes });
       } else {
@@ -82,7 +82,7 @@ router.post('/campaign-product-type', async (req, res) => {
         
         const dataD = await sfmcAPI.getCampaignProductTypes(req.body.access_token)
         
-        const campaignsProductTypes = dataD.data;
+        const campaignsProductTypes = dataD.data.items.values;
 
         logger.info(dataD);
 
