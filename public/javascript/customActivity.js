b07ead22e7fbc145c22c9d0296cc9efabe97fecf
TaoufiqBenallah <taoufiq.benallah@gmail.com>
2022-04-03 13:13:20 +0000

    fjfdfj

--- public/javascript/customActivity.js
+++ public/javascript/customActivity.js
@@ -37,6 +37,7 @@ const buttonSettings = {
   enabled: false,
 };
 
+
 // Function for fetch requests
 const makeRequest = async (endpoint, pyld = {}, qrprms = '') => {
   let body;
@@ -74,157 +75,10 @@ const validateForm = function (cb) {
 };
 */
 
-// check if form values are accurate
-const getFormValues = () => {
-  const cmpId = document.getElementById("campaign-id").value;
-  const cmpControlGroup = document.getElementById("control-group").value;
-  const cmpTypeOffer = document.getElementById("types-of-offers-k").value;
-  const cmpName = document.getElementById("campaign-name").value;
-  const cmpCommunictionType = document.getElementById("communication-type-k").value;
-  const cmpProductType = document.getElementById("types-of-products-k").value;
-  const cmpGroupCmp = document.getElementById("group-campaign-k").value;
-
-  const notEmptyOrUndefined = (value) => {
-    return value !== "" && value !== undefined && value !== null
-  }
-
-  const isValid = notEmptyOrUndefined(cmpId) && notEmptyOrUndefined(cmpControlGroup) && notEmptyOrUndefined(cmpTypeOffer) && notEmptyOrUndefined(cmpName) && notEmptyOrUndefined(cmpCommunictionType) && notEmptyOrUndefined(cmpProductType) && notEmptyOrUndefined(cmpGroupCmp);
-
-  return {
-    isValid, payload: { cmpId, cmpControlGroup, cmpTypeOffer, cmpName, cmpCommunictionType, cmpProductType, cmpGroupCmp }
-  }
-}
-
-// take values from payload and display them in UI
-const mapValuesinUI = (map) => {
-
-  const { campaignId, campaignControlGroup, campaignOffersType, campaignName, campaignCommunicationsType, campaignProductsType, campaignGroup } = map;
-  
-  document.getElementById("campaign-id").value = campaignId;
-  document.getElementById("control-group").value = campaignControlGroup;
-  document.getElementById("types-of-offers-k").value = campaignOffersType;
-  document.getElementById("campaign-name").value = campaignName;
-  document.getElementById("communication-type-k").value = campaignCommunicationsType;
-  document.getElementById("types-of-products-k").value = campaignProductsType;
-  document.getElementById("group-campaign-k").value = campaignGroup;
-
-}
-
-// map dropdown values
-const mapDropdownValues = (element, options) => {
-  if(!options.data.length || !element) return;
-  
-  element.innerHTML = "";
-  for(let i=0; i<options.data.length; i++){
-    let option = document.createElement("div");
-    option.textContent = options.data[i].values.value;
-
-    option.addEventListener("click", function(event){
-      const valueSelected = event.target.textContent;
-      const searchBox = event.target.parentNode;
-      const searchInput = event.target.parentNode.parentNode.getElementsByClassName("form__field--text")[0];
-      searchInput.value = valueSelected;
-      searchBox.classList.add("inactive");
-    })
-
-    element.appendChild(option);
-  }
-}
-
-const manageSearchClickForStaticFields = (event, type) => {
-  const mappedValued = type === "group-campaign-s" ? groupCampaign : campaignCommunicationsTypes;
-  var element = document.getElementById(type);
-  mapDropdownValues(element, mappedValued)
-  var searchBox = event.target.parentNode.getElementsByClassName("form__field--input-search-box")[0];
-  hideSearchBoxes();
-  searchBox.classList.remove("inactive");
-}
-
 // This logic runs while UI is open
 $(window).ready(() => {
   connection.trigger('ready');
 });
-
-const hideSearchBoxes = () => {
-  var searchBoxes = document.getElementsByClassName("form__field--input-search-box");
-  if(!searchBoxes) return;
-
-  for(let i = 0; i<searchBoxes.length; i++){
-    searchBoxes[i].classList.add("inactive")
-  }
-}
-
-const manageDropDownSearchBox = () =>{
-    var searchBoxes = document.getElementsByClassName("form__field--input-drop");
-    for (let i = 0; i < searchBoxes.length; i++) {
-      
-      let dropdownHasConstantValues = searchBoxes[i].classList.contains("fixed")
-
-      let searchBoxList = searchBoxes[i].getElementsByClassName("form__field--input-search-box")[0];
-      let searchBoxIcon = searchBoxes[i].getElementsByClassName("form__field--icon")[0];
-      
-      searchBoxIcon.addEventListener("click", function (event) {
-        const id = event.target.id;
-        if(id === "communication-type-s" || id === "group-campaign-s") { 
-          manageSearchClickForStaticFields(event, id.substring(0, id.length - 2));
-          return;
-        }
-
-        var campaignOffersTypesDropdown = document.getElementById("types-of-offers")
-        var campaignProductsTypesDropdown = document.getElementById("types-of-products")
-
-        if(campaignOffersTypesDropdown) mapDropdownValues(campaignOffersTypesDropdown, campaignOffersTypes)
-        if(campaignProductsTypesDropdown) mapDropdownValues(campaignProductsTypesDropdown, campaignProductsTypes)
-
-        var searchBox = event.target.parentNode.getElementsByClassName("form__field--input-search-box")[0];
-        hideSearchBoxes();
-        searchBox.classList.remove("inactive");
-
-      })
-
-      let searchBoxInput = searchBoxes[i].getElementsByClassName("form__field--text")[0];
-      
-      // dropdownHasConstantValues
-      // form__field--input-search-box
-
-      if(!dropdownHasConstantValues){
-        
-        searchBoxInput.addEventListener("keyup", function (event) {
-          
-          const offersOrProducts = event.target.id === "types-of-products-k" ? "PRODUCTS":"OFFERS";
-          const mappedData = offersOrProducts === "OFFERS" ? campaignOffersTypes.data : campaignProductsTypes.data;
-
-          const value = event.target.value;
-          const filteredValues = mappedData.filter(function(item){
-            return item.values.value.toLowerCase().startsWith(value.toLowerCase());
-          })
-
-          if(value) {
-            var searchBox = event.target.parentNode.getElementsByClassName("form__field--input-search-box")[0];
-            searchBox.innerHTML = "";
-
-            for(let i=0; i<filteredValues.length; i++){
-              let option = document.createElement("div");
-              option.textContent = filteredValues[i].values.value;
-
-              option.addEventListener("click", function(event){
-                const valueSelected = event.target.textContent;
-                const searchBoxK = event.target.parentNode;
-                const searchInput = event.target.parentNode.parentNode.getElementsByClassName("form__field--text")[0];
-                searchInput.value = valueSelected;
-                searchBoxK.classList.add("inactive");
-              })
-
-              searchBox.appendChild(option);
-            }
-            hideSearchBoxes();
-            searchBox.classList.remove("inactive");
-          }
-        })
-      }
-      searchBoxList.classList.add("inactive");
-    }
-}
 
 // This logic runs when user opens the UI
 connection.on('initActivity', async (data) => {
