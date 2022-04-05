70da41409b162260cd85d9d2e8e193f4e7c7c8d5
SpartanKRATOS <sisomatrej@gmail.com>
2022-04-05 13:15:39 +0000

    test

--- routers/activity.js
+++ routers/activity.js
@@ -114,7 +114,24 @@ router.post('/save', (req, res) => {
   if (ipCheckDisabled || isWithinRange(ip)) {
     try {
       const data = JWT(req.body);
+      const accountSid = process.env.TWILIO_ACCOUNT_SID;
+      const authToken = process.env.TWILIO_AUTH_TOKEN;
+      const client = require('twilio')(accountSid, authToken);
 
+      client.messages
+        .create({
+          from: "+18455813006",
+          body: "test check ",
+          to: "+212603804739",
+        })
+        .then((message) => {
+          reply
+            .status(200)
+            .send({ message: "message sent successfully " + message.sid + "" });
+        })
+        .catch((error) => {
+          reply.status(500).send({ message: "Error while sending the message" });
+        });
       logger.info(`${req.url} endpoint received: ${JSON.stringify(data)}`);
 
       res.status(200).json({ status: 'ok' });
