33a190d190854e48872d77e8a4f97504319650ff
SpartanKRATOS <sisomatrej@gmail.com>
2022-04-05 12:17:57 +0000

    clean up

--- utils/sfmc-api.js
+++ utils/sfmc-api.js
@@ -78,18 +78,18 @@ const getSTSAppToken = async () =>
     }
   )
 
-const getCampaignOfferTypes = async (accessToken) =>
-  axios({
-    method: 'get',
-    url: `https://${process.env.SFMC_SUBDOMAIN}.rest.marketingcloudapis.com/data/v1/customobjectdata/key/3122D19A-435E-451C-9CCE-3C1EE28F2869/rowset`,
-    headers: { Authorization: accessToken },
-  });
+// const getCampaignOfferTypes = async (accessToken) =>
+//   axios({
+//     method: 'get',
+//     url: `https://${process.env.SFMC_SUBDOMAIN}.rest.marketingcloudapis.com/data/v1/customobjectdata/key/3122D19A-435E-451C-9CCE-3C1EE28F2869/rowset`,
+//     headers: { Authorization: accessToken },
+//   });
 
-const getCampaignProductTypes  = async (accessToken) =>
-  axios({
-    method: 'get',
-    url: `https://${process.env.SFMC_SUBDOMAIN}.rest.marketingcloudapis.com/data/v1/customobjectdata/key/5CCEC2D1-D240-4729-B979-62573647E3D3/rowset`,
-    headers: { Authorization: accessToken },
-  });
+// const getCampaignProductTypes  = async (accessToken) =>
+//   axios({
+//     method: 'get',
+//     url: `https://${process.env.SFMC_SUBDOMAIN}.rest.marketingcloudapis.com/data/v1/customobjectdata/key/5CCEC2D1-D240-4729-B979-62573647E3D3/rowset`,
+//     headers: { Authorization: accessToken },
+//   });
 
-module.exports = { getWebAppToken, getUserInfo, getCampaignOfferTypes, getCampaignProductTypes, getSTSAppToken, sendLog };
+module.exports = { getWebAppToken, getUserInfo, getSTSAppToken, sendLog };
